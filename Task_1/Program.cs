7c8c9ebdbf23b73e8b39bbd98ab0647478f4bb7f
XTroll <xtroll@inbox.ru>
2023-07-02 14:28:49 +1000

    C#_Homework_6

---
+++ Task_1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 41:
+// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+void Main()
+{
+    System.Console.Write("Введите массив чисел с клавиатуры: ");
+    int[] array = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+    PositiveNumbers(array, out int N);
+    Console.WriteLine($"Чисел в массиве больше '0' :{N}");
+}
+
+Main();
+
+int PositiveNumbers(int[] array, out int N)
+{
+    N = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            N++;
+        }
+    }
+    return N;
+}
---
+++ Task_2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 43:
+// Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2.
+// Значения b1, k1, b2 и k2 задаются пользователем.
+
+// Например: b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void Main()
+{
+    System.Console.Write("Введите через пробел в указанном порядке b1, k1, b2 и k2: ");
+    double[] array = Array.ConvertAll(Console.ReadLine()!.Split(), double.Parse);
+    XY(array, out double X, out double Y);
+    System.Console.WriteLine($"Координата пересечения: X {X}");
+    System.Console.WriteLine($"Координата пересечения: Y {Y}");
+}
+
+Main();
+
+void XY(double[] array, out double X, out double Y)
+{
+    X = (array[2] - array[0]) / (array[1] - array[3]);
+    Y = array[1] * X + array[0];
+}
